e28617f025c85fffa8b8f9287bc3bf448a36bc2e
Toño Cruz <tonchmx@gmail.com>
2019-08-09 19:51:29 -0500

    :sparkles: brands

--- src/components/Brands.vue
+++ src/components/Brands.vue
@@ -1,13 +1,40 @@
 <template>
-  <p>Brands</p>
+  <div class="container">
+    <p class="has-text-centered">
+      <b>We Were Featured By</b>
+    </p>
+    <ul class="brands">
+      <li v-for="(brand, index) in brands" :key="index">
+        <img :src="brand.src" :alt="brand.name" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      brands: [
+        { name: 'Panasonic', src: '/images/brand_1.png' },
+        { name: 'Pano Pro', src: '/images/brand_2.png' },
+        { name: 'townwizard', src: '/images/brand_3.png' },
+        { name: 'erodr', src: '/images/brand_4.png' }
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .container {
+    margin-bottom: 4rem;
+  }
+  .brands {
+    text-align: center;
+    margin-top: 1rem;
+    li {
+      display: inline-block;
+    }
+  }
 </style>
