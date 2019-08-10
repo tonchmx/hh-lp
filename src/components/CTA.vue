fb3a1ab9ab3dbf5470ef1bc2e75e4dba5fdf60af
Toño Cruz <tonchmx@gmail.com>
2019-08-09 21:31:34 -0500

    :sparkles: cta. :lipstick: button purple style

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,5 +1,24 @@
 <template>
-  <p>CTA</p>
+  <div class="container">
+    <div class="columns is-centered is-marginless">
+      <div class="column is-half-desktop">
+        <h2 class="title is-2 has-text-centered">
+          Time is Money. Stop Wasting it.
+        </h2>
+        <p class="has-text-centered">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+          porro quas minima optio! Vel nam, a sapiente sunt porro atque est
+          laudantium, molestias rem, non magnam laborum assumenda aspernatur
+          doloremque?
+        </p>
+        <p class="has-text-centered">
+          <a href="#" class="button is-rounded is-large is-purple has-shadown has-text-centered">
+            Suscribe Now
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +27,12 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .container {
+    margin-bottom: 4rem;
+  }
 
+  h2, p {
+    margin-bottom: 2rem;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import 'bulma/css/bulma.min.css';
+import './styles/app.css';
 
 Vue.config.productionTip = false;
 
